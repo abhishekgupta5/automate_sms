ade65a7f93acd10123afc80d70ea59c0f9287aa0
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-03-02 02:00:50 +0530

    twilio functionality added

--- app.py
+++ app.py
@@ -8,3 +8,17 @@ def check_app():
     return Response("App working!"), 200
 
 
+
+@app.route("/twilio", methods=["POST"])
+def inbound_sms():
+    response=twiml.response
+    inbound_message = request.form.get("Body")
+    if inbound_message == "Hello":
+        response.message("Hello back to you!")
+    else:
+        response.message("Not quite sure what you wrote. Hello anyway!")
+    return Response(str(response), mimetype="application/xml"), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
